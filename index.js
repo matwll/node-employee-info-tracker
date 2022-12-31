f06c10c4b848101c4f3d0f4df8299b310a36d7c1
Matthew Williams <maw01091989@gmail.com>
2022-12-30 18:06:24 -0800

    added promts for the add employees, department, role, and update employee options

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const cTable = require("console.table");
 const connection = require('./config/connection');
+const mysql = require('mysql2');
 
-console.log(connection.query('show tables'));
+console.log(connection.query('show department'));
 
-let main = inquirer.prompt([
+inquirer.prompt([
   {
     type: "list",
     name: "choice",
@@ -22,22 +23,82 @@ let main = inquirer.prompt([
     ],
   },
 ]).then(answer => {
-    console.log(answer);
+    if(answer.choice === 'View all departments'){
+      connection.query('show department');
+  }else if(answer.choice === 'View all roles'){
+      connection.query('show role');
+  }else if(answer.choice === 'View all employees'){
+      connection. query('show employee');
+  }else if(answer.choice === 'Add a department'){
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Enter the name of the department'
+        },
+      ]).then(answers => {
 
-    if(choice.choice === 'View all departments'){
-      connection.execute('show department');
-  }else if(choice.choice === 'View all roles'){
-      connection.execute('show role');
-  }else if(choice.choice === 'View all employees'){
-      connection.execute('show employee');
-  }else if(choice.choice === 'Add a department'){
+      })
+  }else if(answer.choice === 'Add a role'){
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of the role'
+      },
+      {
+        type: 'number',
+        name: 'salary',
+        message: 'Enter the salary of the role'
+      },
+      {
+        type: 'input',
+        name: 'department',
+        message: 'Enter the department of the role'
+      },
+    ]).then(answers => {
+
+    })
+  }else if(answer.choice === 'Add an employee'){
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'fname',
+        message: 'Enter the employees First name.'
+      },
+      {
+        type: 'input',
+        name: 'lname',
+        message: 'Enter the employees Last name.'
+      },
+      {
+        type: 'input',
+        name: 'role',
+        message: 'Enter the employees role.'
+      },
+      {
+        type: 'input',
+        name: 'manager',
+        message: 'Enter the Manager this employee reports to.'
+      },
+    ]).then(answers => {
       
-  }else if(choice.choice === 'Add a role'){
+    })
+  }else if(answer.choice === 'Update an employee role'){
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'employee',
+        message: 'Which employee do you want to update?'
+      },
+      {
+        type: 'input',
+        name: 'newrole',
+        message: 'Enter the new role for this employee.'
+      },
+    ]).then(answers => {
       
-  }else if(choice.choice === 'Add an employee'){
-      
-  }else if(choice.choice === 'Update an employee role'){
-      
+    })
   }else {
   
   }
